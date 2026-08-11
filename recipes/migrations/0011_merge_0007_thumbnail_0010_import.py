480f735539e84a7a247880bc87525416321878f7
Oliver Tressler <oliver.tressler@proton.me>
2026-08-11 15:54:38 +0200

    fix build issues

---
+++ recipes/migrations/0011_merge_0007_thumbnail_0010_import.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("recipes", "0007_recipe_thumbnail"),
+        ("recipes", "0010_recipeimport"),
+    ]
+
+    operations = []
